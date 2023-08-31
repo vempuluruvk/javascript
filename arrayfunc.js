0412e1a7feff0eb0eb6eb038a78cd0e97c6d60eb
vempuluruvk <142653652+vempuluruvk@users.noreply.github.com>
2023-08-31 11:24:30 +0530

    Add files via upload

---
+++ arrayfunc.js
@@ -0,0 +1,21 @@
+const students= newarray[ "vamsi","surya","hemanth","prasad"]
+let a=students[0]
+students[2]="naveen"//push an element into list of values
+students.push["khbeiyg"]
+students.length
+students.pop["jgdvugt"]
+students.slice
+// function regular
+function function_name()  
+{
+//block of code
+}
+function_name();
+// use => this symbol for arrow functions in a single line code
+const add = (a, b) => a + b;
+console.log(add(5, 3));
+
+function showname( name ,age ){
+    alert("hi this is my","+name","+age")
+}
+showname(vamsi ,25);
---
+++ task1.js
@@ -0,0 +1,29 @@
+var age=26;
+let name= "vamsi";
+
+if (age<25){
+  console.log(" eligible for jobs");
+}else if(age>25){
+    console.log("not eligible for jobs");
+}else{
+console.log("last chance for jobs");
+}
+
+
+for(i=0;i<name.length; i++);
+{
+  console.log(name);
+}
+
+let g = 0;
+while (age <= 30) {
+    console.log(name);
+    g++;
+}
+
+let j = 0;
+do {
+    console.log(name);
+    j++;
+} while (j<=27
+   )
